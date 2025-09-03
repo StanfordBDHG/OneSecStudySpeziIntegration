8c0012ab2928621ef8bb8cc3f22e0b1580fd3fa7
Lukas Kollmer <hey@lukaskollmer.de>
2025-09-03 18:42:00 +0200

    update SpeziFoundation dependency

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/StanfordBDHG/OneSecStudySpeziIntegrationInterface.git", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/StanfordSpezi/Spezi.git", from: "1.9.2"),
-        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", from: "2.2.1"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", from: "2.4.0"),
         .package(url: "https://github.com/StanfordSpezi/SpeziHealthKit.git", from: "1.2.3"),
         .package(url: "https://github.com/StanfordBDHG/HealthKitOnFHIR.git", from: "1.1.2"),
         .package(url: "https://github.com/StanfordSpezi/SpeziStorage.git", from: "2.1.1")
